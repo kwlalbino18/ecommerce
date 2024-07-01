78a4a425218b16cff30692072b21f1a70c3f5752
kwlalbino18 <kwlalbino18@gmail.com>
2024-07-01 01:12:35 -0400

    first commit

---
+++ frondent/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+       <h1 className="bg-secondary font-primary">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
